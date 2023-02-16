f625b9ea50a1f2145fb7b8b4563db65faa5c46ca
Carlos Ferrer <carlosvferrer@gmail.com>
2023-02-16 18:27:22 -0300

    Challange solved

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,28 @@
+class PurchasesController < ApplicationController
+  def new
+  end
+
+  def create
+    file = params[:file].tempfile
+    purchases = []
+    File.readlines(file).each_with_index do |line, index|
+      next if index == 0 # Ignora o cabeçalho
+      fields = line.strip.split("\t")
+      purchase = Purchase.new(
+        purchaser_name: fields[0],
+        item_description: fields[1],
+        item_price: fields[2].to_f,
+        purchase_count: fields[3].to_i,
+        merchant_address: fields[4],
+        merchant_name: fields[5]
+      )
+      purchases << purchase
+    end
+    Purchase.import purchases # Usa o método import do active record para salvar todos os registros de uma vez
+    redirect_to purchase_path(id: 1) # Redireciona para a página que mostra o total de vendas
+  end
+
+  def show
+    @total_gross_income = Purchase.sum("item_price * purchase_count")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :purchases, only: [:new, :create, :show]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'purchases#new'
 end
